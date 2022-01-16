d0d4e191c33ed6ceb38dfbb39354f7b065e37391
John Soter <john.soter@outlook.com>
2022-01-15 21:11:33 -0500

    typo

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,13 +3,13 @@ import { useRouter } from 'next/router'
 
 import GolfCourseNav from '@/components/GolfCourseNav'
 import { useEffect } from 'react'
-import { ADD_GOLE_COURSE } from '@/constants'
+import { ADD_GOLF_COURSE } from '@/constants'
 
 const Home: NextPage = () => {
   const router = useRouter();
 
   useEffect(() => {
-    router.push(ADD_GOLE_COURSE);
+    router.push(ADD_GOLF_COURSE);
   }, []);
 
   return (
